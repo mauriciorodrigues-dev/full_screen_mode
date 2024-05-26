38847b9b7cd54188642481615f9b5ff0219b3578
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-05-26 16:22:08 -0300

    update

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 function entrarModoTelaCheia() {
   var elemento = document.documentElement;
-  let opt = document.querySelector('.opt')
+
 
   if (elemento.requestFullscreen) {
-      elemento.requestFullscreen();
-      opt.display = 'none'
-  } else if (elemento.mozRequestFullScreen) { /* Firefox */
-      elemento.mozRequestFullScreen();
-  } else if (elemento.webkitRequestFullscreen) { /* Chrome, Safari e Opera */
-      elemento.webkitRequestFullscreen();
-  } else if (elemento.msRequestFullscreen) { /* IE/Edge */
-      elemento.msRequestFullscreen();
+    elemento.requestFullscreen();
+
+  } else if (elemento.mozRequestFullScreen) {
+    /* Firefox */
+    elemento.mozRequestFullScreen();
+  } else if (elemento.webkitRequestFullscreen) {
+    /* Chrome, Safari e Opera */
+    elemento.webkitRequestFullscreen();
+  } else if (elemento.msRequestFullscreen) {
+    /* IE/Edge */
+    elemento.msRequestFullscreen();
   }
 }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js");
+}
 
-window.onload = function() {
+window.onload = function () {
   var botao = document.querySelector("button");
   botao.click();
-}
+};
