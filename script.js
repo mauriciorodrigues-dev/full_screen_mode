4277cc10cc636ead9bcbdb0936a62bbeaa6db384
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-05-26 16:15:09 -0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function entrarModoTelaCheia() {
   var elemento = document.documentElement;
+  let opt = document.querySelector('.opt')
 
   if (elemento.requestFullscreen) {
       elemento.requestFullscreen();
+      opt.display = 'none'
   } else if (elemento.mozRequestFullScreen) { /* Firefox */
       elemento.mozRequestFullScreen();
   } else if (elemento.webkitRequestFullscreen) { /* Chrome, Safari e Opera */
